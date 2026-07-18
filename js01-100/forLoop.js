b68d45cc74bc639816602eb1b0482d64ff11a537
sajal-mazumder <sajalmazumder47@gmail.com>
2026-07-18 23:45:01 +0600

    some update for loop

---
+++ js01-100/forLoop.js
@@ -0,0 +1,11 @@
+// console.log('for loop');
+
+// for loop practicing start 
+
+let sum = 0;
+
+for( let i = 1; i <= 10; i++){
+    // console.log(i);
+    console.log( sum + ' + ' + i + ' = ' + (sum + i));
+    sum = sum + i;
+}
